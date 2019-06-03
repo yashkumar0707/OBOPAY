ae3866d49d4834a0186d058c2f413183a1c98282
yashkumar0707 <yashkumar0707@gmail.com>
2019-06-03 12:07:17 +0530

    js testing

---
+++ webtest/WebContent/javascript/HELLOEXT.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+document.write("EXTERNAL ");
+var a= prompt("enter a number");
+var b="yash";
+document.write(a+" "+b);
+if (a==10)
+	document.write("<h1>A IS EQUAL TO 10 </h1>\n");
+else
+	document.write("A is not equal to 10");
+switch (a)
+{	case "1":
+	document.write("A is not equal to 10");
+	case "10":
+		document.write("switch");
+}
